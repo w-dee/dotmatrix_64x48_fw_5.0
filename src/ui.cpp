b53c21bfc204a430dbe7a4d3f996d68150cfa850
W.Dee <dee@kikyou.info>
2021-04-12 15:15:34 +0900

    Removed test comment-out

--- src/ui.cpp
+++ src/ui.cpp
@@ -1419,10 +1419,10 @@ void ui_setup()
 
 	screen_clock = new screen_clock_t();
 
-//	if(button_get_scan_bits() & BUTTON_UP)
+	if(button_get_scan_bits() & BUTTON_UP)
 		screen_manager.push(new screen_led_test_t());
-//	else
-//		screen_manager.push(screen_clock);
+	else
+		screen_manager.push(screen_clock);
 }
 
 void ui_process()
