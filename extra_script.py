46df48a4123ef30ae9a672d45279510f2f9066d4
W.Dee <dee@kikyou.info>
2021-04-22 16:59:44 +0900

    unified two similar upload functions

--- extra_script.py
+++ extra_script.py
@@ -1,11 +1,9 @@
 Import("env")
 import make_archive
 
-# custom target "uploadba" to upload bad apple movie demo
-def uploadba(*args, **kwargs):
-    # note: keep that this font start address and the address written in custom.csv are in sync.
-    # note: this may destory font partition.
-    env.Replace(FONT_ADDRESS="0x890000", FONT_FILE_NAME="work-priv/BadApple/BA_64x48_comp.bin")
+# upload helper function
+def extra_upload(address, filename):
+    env.Replace(FONT_ADDRESS=address, FONT_FILE_NAME=filename)
     env.AutodetectUploadPort()
     env.Replace(
         UPLOADERFLAGS=[
@@ -20,28 +18,19 @@ def uploadba(*args, **kwargs):
             "$FONT_ADDRESS", "$FONT_FILE_NAME"
         ],
     UPLOADCMD='"$PYTHONEXE" "$UPLOADER" $UPLOADERFLAGS $SOURCE' )
-    env.Execute(env["UPLOADCMD"])
+    env.Execute(env["UPLOADCMD"]) 
+
+# custom target "uploadba" to upload bad apple movie demo
+def uploadba(*args, **kwargs):
+    # note: keep that this font start address and the address written in custom.csv are in sync.
+    # note: this may destory font partition.
+    extra_upload("0x890000", "work-priv/BadApple/BA_64x48_comp.bin")
 
 # custom target "uploadfont" to upload font file
 def uploadfont(*args, **kwargs):
     # note: keep that this font start address and the address written in custom.csv are in sync.
     # TODO: take the address automatically from the csv file
-    env.Replace(FONT_ADDRESS="0x890000", FONT_FILE_NAME="src/fonts/TakaoPGothicC.ttf")
-    env.AutodetectUploadPort()
-    env.Replace(
-        UPLOADERFLAGS=[
-            "--chip", "esp32",
-            "--port", '"$UPLOAD_PORT"',
-            "--baud", "$UPLOAD_SPEED",
-            "--before", "default_reset",
-            "--after", "hard_reset",
-            "write_flash", "-z",
-            "--flash_mode", "$BOARD_FLASH_MODE",
-            "--flash_size", "detect",
-            "$FONT_ADDRESS", "$FONT_FILE_NAME"
-        ],
-    UPLOADCMD='"$PYTHONEXE" "$UPLOADER" $UPLOADERFLAGS $SOURCE' )
-    env.Execute(env["UPLOADCMD"])
+    extra_upload("0x890000", "src/fonts/TakaoPGothicC.ttf")
 
 
 env.AddCustomTarget(name="uploadfont",
